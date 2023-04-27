547dabd9ec15e641c8bc1857b848b1cdf8a12c83
chithiraivel <chithiraivels11@gmail.com>
2023-04-27 21:27:58 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import About from './Components/About'
+import Experience from './Components/Experience'
+import Tech from './Components/Tech'
+import Works from './Components/Works'
+import Feedbacks from './Components/Feedbacks'
+import Contact from './Components/Contact'
+import StarsCanvas from './Components/StarsCanvas'
+
+
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+       <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+              <Navbar/>
+              <Hero/>
+          </div>
+          <About/>
+          <Experience/>
+          <Tech/>
+          <Works/>
+          <Feedbacks/>
+
+          <div className="relative z-0">
+            <Contact/>
+            <StarsCanvas/>
+            
+          </div>
+
+       </div>
+    </BrowserRouter>
   );
 }
 
